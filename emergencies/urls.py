3705c443063df276c1d91c4e5e7da8500b25005d
barnanova <olanrewajupelumi606@gmail.com>
2025-12-22 11:22:09 +0100

    Remove Agora token URL

--- emergencies/urls.py
+++ emergencies/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from .views import ReceiveEmergency
-from .views import generate_agora_token
 
 urlpatterns = [
     path('receive-emergency/', ReceiveEmergency.as_view(), name='receive-emergency'),
-    path('agora-token/<str:report_id>/', generate_agora_token, name='agora-token'),
 ]
